4053b033bb685b897f8f2b34a3c22b00154aa262
tanay <tanay.sen1212@gmail.com>
2025-08-07 11:51:21 +0530

    Initial spring boot project structure

---
+++ currency-exchange-service/src/test/java/com/tanay/learning/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanay.learning.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
