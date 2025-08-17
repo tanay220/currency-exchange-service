dd0ba84d123f2dee4ad5166a52129cd87cf95b82
tanay <tanay.sen1212@gmail.com>
2025-08-18 00:13:48 +0530

    Added spring cloud API gateway and Eureka discovery server microservice

---
+++ api-gateway/src/main/java/com/tanay/learning/api_gateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.tanay.learning.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion-service"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)", "/currency-conversion/${segment}"))
+                        .uri("lb://currency-conversion-service"))
+                .route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange-service"))
+                .build();
+    }
+}
--- currency-conversion-service/src/main/java/com/tanay/learning/currency_conversion_service/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/tanay/learning/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
 
